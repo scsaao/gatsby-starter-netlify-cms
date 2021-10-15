b041d35afc954d382bff86a8ba610bb7eee8a6c1
Sunil Chandra <87673399+scsaao@users.noreply.github.com>
2021-10-15 14:48:34 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,7 +19,8 @@ const Footer = class extends React.Component {
           />
         </div>
         <div className="content has-text-centered has-background-danger-light has-text-warning-dark">
-          <div className="container has-background-white has-text-black-ter">
+          <!-- div className="container has-background-white has-text-black-ter" -->
+          <div className="container">  
             <div style={{ maxWidth: '100vw' }} className="columns">
               <div className="column is-4">
                 <section className="menu">
