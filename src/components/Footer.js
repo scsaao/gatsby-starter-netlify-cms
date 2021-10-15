c6ed6aa141078b61f7fd0f71c9992bd38ce1126a
Sunil Chandra <87673399+scsaao@users.noreply.github.com>
2021-10-15 15:22:43 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,7 @@ import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
 import vimeo from '../img/social/vimeo.svg'
+import telegram from '../img/social/telegram.svg'
 
 const Footer = class extends React.Component {
   render() {
